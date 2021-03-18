9b8196e6cd81d02b731e72c0bb1397863ad2220d
deborahoni <34198791+deborahoni@users.noreply.github.com>
2021-03-17 23:25:02 -0400

    authenticating users

---
+++ app/user/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import generics
+from user.serializers import UserSerializer, AuthTokenSerializer
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    """below sets the renderer so we can view the renderer in the endpoint to view request"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+ 
