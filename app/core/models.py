b39512d20beea290766f26a19efb54dd0a796694
deborahoni <34198791+deborahoni@users.noreply.github.com>
2021-03-28 23:15:09 -0400

    added a recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -57,3 +57,20 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+class recipe(models.Model):
+    """recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits = 5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    """ManytoMany field to ingredient and tags"""
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+    
+    def __str__ (self):
+        return self.title
--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -59,3 +59,13 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """ test the recipe strin representation"""
+        recipe = models.recipe.objects.create(
+            user = sample_user(),
+            title = 'steak and mushroom sauce',
+            time_minutes = 5,
+            price = 5.00
+        )
+        self.assertEqual(str(recipe),recipe.title)
